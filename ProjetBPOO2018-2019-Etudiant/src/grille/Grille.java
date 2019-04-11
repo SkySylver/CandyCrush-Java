48225158e6f9778016d2d9e44058bf07b087dba8
Enes <Enes@DESKTOP-H8P0RDE>
2019-04-11 03:03:40 +0200

    11

--- ProjetBPOO2018-2019-Etudiant/src/grille/Grille.java
+++ ProjetBPOO2018-2019-Etudiant/src/grille/Grille.java
@@ -1,24 +1,36 @@
 package grille;
 
 import bonbon.Bonbon;
+import bonbon.BonbonSimple;
 
 public class Grille {
 	private final static int taille = 10;
 
-	private Bonbon[][] g;
+	private Bonbon[][] g = new Bonbon[taille][taille];
 
 	public Grille() {
 
 		for (int i = 0; i <= taille - 1; i++) {
 			for (int j = 0; j <= taille - 1; j++) {
 				do {
-				
-				}while(g[i][j].Verifier());
-				
+					g[i][j] = new BonbonSimple();
+				}while(Verifier(g, i, j));
 			}
 
 		}
-
+		
+	}
+	
+	public boolean Verifier(Bonbon[][] grille, int i, int j) {
+		return false;
+		
+		//Verifier ligne
+		//Verifier colonne
+		
 	}
 
+	public void Echanger() {
+		
+		
+	}
 }
