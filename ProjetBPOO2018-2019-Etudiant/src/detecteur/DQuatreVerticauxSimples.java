81dccb90333d46c30873c758829d8f8fe7ff6a07
emincan <emin_can@live.fr>
2019-05-27 05:47:25 +0000

    Upload New File

---
+++ ProjetBPOO2018-2019-Etudiant/src/detecteur/DQuatreVerticauxSimples.java
@@ -0,0 +1,57 @@
+package detecteur;
+
+import combinaison.Combinaison;
+import combinaison.QuatreVerticauxSimples;
+import exceptions.CandyException;
+import grille.Grille;
+
+//QUATRE BONBONS SIMPLES ALIGNES VERTICALEMENT
+public class DQuatreVerticauxSimples extends Detecteur {
+	
+
+	private int debutLigne;
+	private int ligne;
+	private int colonne;
+
+	public DQuatreVerticauxSimples(Detecteur d) {
+		super(d);
+	}
+
+	public Combinaison combinaisonExiste(int l, int c, Grille grille) throws CandyException {
+		// même principe que QuatreHorizontauxSimples
+
+		ligne = l;
+		colonne = c;		
+		String coul = grille.getCouleur(l, c);
+		String t = grille.getType(l, c);
+
+		if (t.equals("BonbonSimple")) {
+
+			while (l > 0 && grille.getCouleur(l - 1, c).equals(coul) && grille.getType(l - 1, c).equals(t)) {
+				l--;
+			}
+
+			if (l < 8) {
+				if (grille.getCouleur(l + 1, c).equals(coul) && grille.getCouleur(l + 2, c).equals(coul)
+						&& grille.getCouleur(l + 3, c).equals(coul)) {
+					if (grille.getType(l + 1, c).equals(t) && grille.getType(l + 2, c).equals(t)
+							&& grille.getType(l + 3, c).equals(t)) {
+						debutLigne = l;
+						return new QuatreVerticauxSimples(debutLigne, ligne, colonne);
+					}
+				}
+			}
+		}
+
+		return null;
+	}
+
+	public void reponse(Grille grille) throws CandyException {
+		System.out.println("4 bonbons " + grille.getCouleur(debutLigne, colonne));
+	}
+
+	@Override
+	public String toString() {
+		return "QuatreVerticauxSimples [debutLigne=" + debutLigne + ", colonne=" + colonne + "]";
+	}
+}
