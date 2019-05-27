ccaf7766c7127f23e42e9c5787754520f0661cfd
emincan <emin_can@live.fr>
2019-05-27 05:47:42 +0000

    Upload New File

---
+++ ProjetBPOO2018-2019-Etudiant/src/detecteur/DTroisHorizontauxRayesH.java
@@ -0,0 +1,54 @@
+package detecteur;
+
+import combinaison.Combinaison;
+import combinaison.TroisHorizontauxRayesH;
+import exceptions.CandyException;
+import grille.Grille;
+
+// TROIS BONBONS ALIGNES HORIZONTALEMENT DONT UN RAYE A L'HORIZONTALE
+public class DTroisHorizontauxRayesH extends Detecteur {
+	private int debutColonne;
+	private int ligne;
+
+	public DTroisHorizontauxRayesH(Detecteur d) {
+		super(d);
+	}
+	
+	public Combinaison combinaisonExiste(int l, int c, Grille grille) throws CandyException {
+
+		ligne = l;
+		String coul = grille.getCouleur(l, c);
+		String t = grille.getType(l, c);
+
+		if (t.equals("BonbonSimple") || t.contentEquals("BonbonHorizontal")) {
+
+			// on se place sur le bonbon le plus à gauche qui est de la même couleur
+			while (c > 0 && grille.getCouleur(l, c - 1).equals(coul)) {
+				c--;
+			}
+
+			if (c < 8) {
+				if (grille.getCouleur(l, c + 1).equals(coul) && grille.getCouleur(l, c + 2).equals(coul)) {
+					if (grille.getType(l, c).equals("BonbonHorizontal")
+							|| grille.getType(l, c + 1).equals("BonbonHorizontal")
+							|| grille.getType(l, c + 2).equals("BonbonHorizontal")) {
+						debutColonne = c;// cette ligne est utile seulement pour connaître la couleur car
+						// de toute façon on va rayer toute la ligne
+						return new TroisHorizontauxRayesH(ligne);
+					}
+				}
+			}
+		}
+
+		return null;
+	}
+
+	public void reponse(Grille grille) throws CandyException {
+		System.out.println("3 bonbons " + grille.getCouleur(ligne, debutColonne) + " dont un rayé à l'horizontal");
+	}
+
+	@Override
+	public String toString() {
+		return "TroisHorizontauxRayesH [debutColonne=" + debutColonne + ", ligne=" + ligne + "]";
+	}
+}
