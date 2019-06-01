c66955ab4157750cb5f78b075b87ebb412654b5e
Enes Usta <mine.skysylver@gmail.com>
2019-06-01 00:39:23 +0000

    Upload New File

---
+++ ProjetBPOO2018-2019-Etudiant/src/interf/KeyFrame3.java
@@ -0,0 +1,29 @@
+package interf;
+
+import exceptions.CandyException;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public final class KeyFrame3 implements EventHandler<ActionEvent> {
+	private Controller con;
+
+	public KeyFrame3(Controller c) {
+		con = c;
+	}
+
+	public void handle(ActionEvent event) {
+		
+
+		con.getGrille().tomber();
+		con.getGc().clearRect(0, 0, 640, 640);
+		try {
+			con.dessinerPlateau();
+		} catch (CandyException e) {
+			e.printStackTrace();
+		}
+		// Dans cette frame, on est sensÃ© voir des Ã©toiles qui matÃ©rialisent la
+		// prochaine
+		// disparition des bonbons (etoile = case vide)
+
+	}
+}
