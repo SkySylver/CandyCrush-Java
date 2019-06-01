3f6417134c1aaba38c7acb47f3d61b38c730628e
Enes Usta <mine.skysylver@gmail.com>
2019-06-01 14:38:32 +0000

    Upload New File

---
+++ ProjetBPOO2018-2019-Etudiant/src/interf/KeyFrame4.java
@@ -0,0 +1,21 @@
+package interf;
+
+import exceptions.CandyException;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public final class KeyFrame4 implements EventHandler<ActionEvent> {
+	private Controller con;
+
+	public KeyFrame4(Controller c) {
+		con = c;
+	}
+
+	public void handle(ActionEvent event) {
+		if (con.isFin()) {
+			con.afficherFin();
+		}
+		con.getGc().clearRect(0, 0, 640, 640);
+
+	}
+}
